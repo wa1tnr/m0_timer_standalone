4542f5ef4fe446d3624b4c0d6cf66007d03fb297
wa1tnr <wa1tnr@users.noreply.github.com>
2018-06-23 21:03:20 +0000

    modified:   m0_timer_standalone.ino

--- m0_timer_standalone.ino
+++ m0_timer_standalone.ino
@@ -1,5 +1,7 @@
-// Sat 23 Jun 18:50:05 UTC 2018
-// 0105-a0a-01-
+// Sat 23 Jun 18:51:12 UTC 2018
+// 0105-a0a-02-
+
+// kanicu
 
 #include <Arduino.h>
 #include "neo_pixel.h"
@@ -10,7 +12,7 @@
 #undef  WAITFOR // do we wait for a serial connection or not?
 
 /******************************************************************************/
-/**  The Arduino M0 pro has 0x4000 as bootloader offset                      **/
+/**  Simple state machine to toggle an LED from inside an ISR                **/
 /******************************************************************************/
 
 /* ideas sourced from MartinL on forum.arduino.cc */
@@ -23,17 +25,17 @@ void tickOn(void) { }
 void tick(void) { }
 
 void darkenlight(void) {
-  digitalWrite(LED, 0); // turn off
+    digitalWrite(LED, 0); // turn off
 }
 
 
 void brightenlight(void) {
-  digitalWrite(LED, 1); // turn on
+    digitalWrite(LED, 1); // turn on
 }
 
 void blinkenlight(void) {
-  brightenlight();
-  darkenlight();
+    brightenlight();
+    darkenlight();
 }
 
 void introduction() {
@@ -48,99 +50,94 @@ void introduction() {
     Serial.println(" (end of truncated introduction.  Bye.)");
 }
 
-void iblinki(void) {
+void iblinki(void) { // only during WAITFOR
     brightenlight();
     delay(30);
     darkenlight();
     delay(800);
 }
 
-
 void setup() {
-  pinMode(LED, 1); // OUTPUT
+    pinMode(LED, 1); // OUTPUT
 
-  Serial.begin(38400); // Open serial communications
+    Serial.begin(38400); // Open serial communications
 
-  delay(2000); // forced 2 second delay -- omit if not desired.
+    delay(2000); // forced 2 second delay -- omit if not desired.
 
 #ifdef WAITFOR // we want to wait for a serial connection
-  while(!Serial) { // hold for connection
-      iblinki(); delay(300);
-  }
+    while(!Serial) { // hold for connection
+        iblinki(); delay(300); // optional blinkie with timed delays
+    }
 #endif // #ifdef WAITFOR
 
-  // ANSI escape sequence
-  //  (yellow text in VT220 terminal:
+    // ANSI escape sequence
+    //  (yellow text in VT220 terminal:
 
-  Serial.print("\033\133"); // ESC [
-  Serial.print("\063\063"); // 33 - yellow fg
-  Serial.print("m");        // for the stanza
+    Serial.print("\033\133"); // ESC [
+    Serial.print("\063\063"); // 33 - yellow fg
+    Serial.print("m");        // for the stanza
 
+    // CLOCK
 
-  darkenlight();
-  blinkenlight();
-  darkenlight();
+    REG_GCLK_GENDIV = GCLK_GENDIV_DIV(0xfd) |       // Divide the 48MHz clock source by divisor 3: 48MHz/3=16MHz
+                      GCLK_GENDIV_ID(4);            // Select Generic Clock (GCLK) 4
+    while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
 
+    REG_GCLK_GENCTRL = GCLK_GENCTRL_IDC |           // Set the duty cycle to 50/50 HIGH/LOW
+                       GCLK_GENCTRL_GENEN |         // Enable GCLK4
+                       GCLK_GENCTRL_SRC_DFLL48M |   // Set the 48MHz clock source
+                       GCLK_GENCTRL_ID(4);          // Select GCLK4
+    while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
 
-  REG_GCLK_GENDIV = GCLK_GENDIV_DIV(0xfd) |       // Divide the 48MHz clock source by divisor 3: 48MHz/3=16MHz
-                    GCLK_GENDIV_ID(4);            // Select Generic Clock (GCLK) 4
-  while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
-
-  REG_GCLK_GENCTRL = GCLK_GENCTRL_IDC |           // Set the duty cycle to 50/50 HIGH/LOW
-                     GCLK_GENCTRL_GENEN |         // Enable GCLK4
-                     GCLK_GENCTRL_SRC_DFLL48M |   // Set the 48MHz clock source
-                     GCLK_GENCTRL_ID(4);          // Select GCLK4
-  while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
-
-  REG_GCLK_CLKCTRL = GCLK_CLKCTRL_CLKEN |         // Enable GCLK4 to TC4 and TC5
-                     GCLK_CLKCTRL_GEN_GCLK4 |     // Select GCLK4
-                     GCLK_CLKCTRL_ID_TC4_TC5;     // Feed the GCLK4 to TC4 and TC5
-  while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
+    REG_GCLK_CLKCTRL = GCLK_CLKCTRL_CLKEN |         // Enable GCLK4 to TC4 and TC5
+                       GCLK_CLKCTRL_GEN_GCLK4 |     // Select GCLK4
+                       GCLK_CLKCTRL_ID_TC4_TC5;     // Feed the GCLK4 to TC4 and TC5
+    while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
  
-  REG_TC4_CTRLA |= TC_CTRLA_MODE_COUNT8;          // Set the counter to 8-bit mode
-  while (TC4->COUNT8.STATUS.bit.SYNCBUSY);        // Wait for synchronization
+    REG_TC4_CTRLA |= TC_CTRLA_MODE_COUNT8;          // Set the counter to 8-bit mode
+    while (TC4->COUNT8.STATUS.bit.SYNCBUSY);        // Wait for synchronization
 
-  REG_TC4_COUNT8_CC0 = 0x02;                      // TC4 CC0 (arbitrary value)
+    REG_TC4_COUNT8_CC0 = 0x02;                      // TC4 CC0 (arbitrary value)
 
-  while (TC4->COUNT8.STATUS.bit.SYNCBUSY);        // Wait for synchronization
-  REG_TC4_COUNT8_CC1 = 0x02;                      // TC4 CC1 (arbitrary value)
+    while (TC4->COUNT8.STATUS.bit.SYNCBUSY);        // Wait for synchronization
+    REG_TC4_COUNT8_CC1 = 0x02;                      // TC4 CC1 (arbitrary value)
 
-  while (TC4->COUNT8.STATUS.bit.SYNCBUSY);        // Wait for synchronization
-  REG_TC4_COUNT8_PER = 0xec;                      // Set the PER (period) register to its maximum value
-  while (TC4->COUNT8.STATUS.bit.SYNCBUSY);        // Wait for synchronization
+    while (TC4->COUNT8.STATUS.bit.SYNCBUSY);        // Wait for synchronization
+    REG_TC4_COUNT8_PER = 0xec;                      // Set the PER (period) register to its maximum value
+    while (TC4->COUNT8.STATUS.bit.SYNCBUSY);        // Wait for synchronization
 
-  NVIC_SetPriority(TC4_IRQn, 0);    // Set the Nested Vector Interrupt Controller (NVIC) priority for TC4 to 0 (highest)
-  NVIC_EnableIRQ(TC4_IRQn);         // Connect TC4 to Nested Vector Interrupt Controller (NVIC)
+    NVIC_SetPriority(TC4_IRQn, 0);    // Set the Nested Vector Interrupt Controller (NVIC) priority for TC4 to 0 (highest)
+    NVIC_EnableIRQ(TC4_IRQn);         // Connect TC4 to Nested Vector Interrupt Controller (NVIC)
 
-  REG_TC4_INTFLAG |= TC_INTFLAG_MC1 | TC_INTFLAG_MC0 | TC_INTFLAG_OVF;        // Clear the interrupt flags
-  REG_TC4_INTENSET = TC_INTENSET_MC1 | TC_INTENSET_MC0 | TC_INTENSET_OVF;     // Enable TC4 interrupts
+    REG_TC4_INTFLAG |= TC_INTFLAG_MC1 | TC_INTFLAG_MC0 | TC_INTFLAG_OVF;        // Clear the interrupt flags
+    REG_TC4_INTENSET = TC_INTENSET_MC1 | TC_INTENSET_MC0 | TC_INTENSET_OVF;     // Enable TC4 interrupts
  
-  // -------------------------------------------------
-  // 
-  //                23 June 2018
-  // 
-  //    To change the blink rate of the LED on D13,
-  //     select a different prescaler here:
-  // 
-  // -------------------------------------------------
+    // -------------------------------------------------
+    // 
+    //                23 June 2018
+    // 
+    //    To change the blink rate of the LED on D13,
+    //     select a different prescaler here:
+    // 
+    // -------------------------------------------------
 
-  // 1024 256 64 16:
+    // 1024 256 64 16:
 
-  //  REG_TC4_CTRLA |= TC_CTRLA_PRESCALER_DIV1024 |   // Set prescaler to 1024
+    //  REG_TC4_CTRLA |= TC_CTRLA_PRESCALER_DIV1024 |   // Set prescaler to 1024
 
-  //  REG_TC4_CTRLA |= TC_CTRLA_PRESCALER_DIV256  |   // Set prescaler to  256
+    //  REG_TC4_CTRLA |= TC_CTRLA_PRESCALER_DIV256  |   // Set prescaler to  256
 
-      REG_TC4_CTRLA |= TC_CTRLA_PRESCALER_DIV64   |   // Set prescaler to   64, 16MHz/64 = 256kHz
+        REG_TC4_CTRLA |= TC_CTRLA_PRESCALER_DIV64   |   // Set prescaler to   64, 16MHz/64 = 256kHz
 
-  //  REG_TC4_CTRLA |= TC_CTRLA_PRESCALER_DIV16   |   // Set prescaler to   16
+    //  REG_TC4_CTRLA |= TC_CTRLA_PRESCALER_DIV16   |   // Set prescaler to   16
 
-                       TC_CTRLA_ENABLE;               // Enable TC4
+                         TC_CTRLA_ENABLE;               // Enable TC4
 
-  while (TC4->COUNT8.STATUS.bit.SYNCBUSY);            // Wait for synchronization
+    while (TC4->COUNT8.STATUS.bit.SYNCBUSY);            // Wait for synchronization
 
-  Serial.println(" pip ");
-  delay(9000);
-  introduction();
+    Serial.println("pip ");
+    delay(9000);
+    introduction();
 }
 
 // 1: index  1, then 2, then -1: darken.
